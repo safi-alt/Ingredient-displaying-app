2acc5fd0c0ca260fd65bc7762463bf527d1b4134
safi <safiazim8@gmail.com>
2020-08-31 17:35:04 +0500

    Created Ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
 
 function Ingredients() {
   return (
