bc42db5b7c94b7927cd106bb97b54396ffaef044
safi <safiazim8@gmail.com>
2020-08-31 17:40:54 +0500

    Setting up Index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import Ingredients from './components/Ingredients/Ingredients';
+import Ingredients from "./components/Ingredients/Ingredients";
 
-const App = props => {
+const App = (props) => {
   return <Ingredients />;
 };
 
