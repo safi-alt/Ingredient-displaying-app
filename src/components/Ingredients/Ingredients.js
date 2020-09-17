c068a670cf22c15e6ebc3a9bc948f2bbc0dcee67
safi <safiazim8@gmail.com>
2020-09-18 01:30:37 +0500

    useEffect to load Data from Firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -6,6 +6,24 @@ import IngredientList from "./IngredientList";
 
 const Ingredients = () => {
   const [userIngriedients, setUserIngriedients] = useState([]);
+
+  useEffect(() => {
+    fetch("https://react-hooks-project-5ea25.firebaseio.com/ingredients.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const loadIngredients = [];
+        for (const key in responseData) {
+          loadIngredients.push({
+            id: key,
+            title: responseData[key].ingredient.title,
+            amount: responseData[key].ingredient.amount,
+          });
+        }
+        setUserIngriedients(loadIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch("https://react-hooks-project-5ea25.firebaseio.com/ingredients.json", {
